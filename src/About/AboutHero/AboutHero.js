19cfdc9f33f06fd79418aa069874d79dfb6f9c63
Prajjwal2404 <prajjwalpratapshah.2404@gmail.com>
2024-10-22 23:24:27 +0530

    updated animation on about page

--- src/About/AboutHero/AboutHero.js
+++ src/About/AboutHero/AboutHero.js
@@ -9,15 +9,15 @@ export default function AboutHero() {
         <div className='about-hero'>
             <div className="about-hero-div">
                 <motion.div className='about-hero-logo'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 1.25, duration: 0.5 }}>
+                    initial={{ scale: 0, originX: 0, originY: "100%" }}
+                    animate={{ scale: 1 }}
+                    transition={{ type: "spring", delay: 0.75 }}>
                     <Logo />
                 </motion.div>
                 <motion.div className='about-hero-welcome'
                     initial={{ scale: 2.5, top: '50%', y: '-50%', right: '50%', x: '80%' }}
                     animate={{ scale: 1, top: '-2.5%', y: 0, right: 0, x: 0 }}
-                    transition={{ delay: 0.5, duration: 0.5 }}>
+                    transition={{ type: "spring", delay: 0.75 }}>
                     <Welcome />
                 </motion.div>
             </div>
